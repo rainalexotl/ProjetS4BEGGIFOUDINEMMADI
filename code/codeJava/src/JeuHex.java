b0aab48dc1b56badb47a2de121f30d19b63e5f7e
Rain-Alexandra BEGG <rainalex01@gmail.com>
2017-05-22 20:48:07 +0200

    commit

--- code/codeJava/src/JeuHex.java
+++ code/codeJava/src/JeuHex.java
@@ -411,7 +411,7 @@ public class JeuHex {
         Player p;
     	int i = 0;
         char event = 'c';
-        System.out.println(board.getNbHexes());
+        
     	while(i < board.getNbHexes() && (event == 'c')){
     		char color = Player.quiJoue(joueur);
 
